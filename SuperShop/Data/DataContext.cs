7d9830d74640be84d83c2e3f0e302a3973dc0df4
vladimiro-fs <vladzyo.gaming@pm.me>
2024-07-19 21:01:27 +0100

    Data and Entities folders created, DataContext and Product classes added; Startup modified; appsettings.json modified;

---
+++ SuperShop/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿namespace SuperShop.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using SuperShop.Data.Entities;
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        /// <summary>
+        /// Injectar o nosso DataContext no DbContextOptions e usar a herança para dizer que o nosso DataContext herda do DbContextOptions
+        /// </summary>
+        /// <param name="options"></param>
+        public DataContext(DbContextOptions<DataContext> options) : base(options)  
+        {                                                                           
+
+        } 
+    }
+}
